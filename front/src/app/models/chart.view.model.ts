c6d82790fc47e80884ff7415ecce97ad7c622bd2
sebferrer <sebastien@sebferrer.fr>
2020-12-06 23:56:26 +0100

    chart view model

---
+++ front/src/app/models/chart.view.model.ts
@@ -0,0 +1,102 @@
+import { ISymptomOverview, ISymptomLog, ISymptom } from './symptom.model';
+import { Observable } from 'rxjs';
+import { DayViewModel } from './day.view.model';
+
+export class DayChartViewModel {
+	public type: string;
+	public title: string;
+	public data: any[][];
+	public columns: string[];
+	public options: any;
+	public timeSymptoms: Map<string, ISymptomOverview[]>;
+	public timeSymptomsOrder: string[];
+	public lastSymptomMap: Map<string, ISymptomLog>;
+
+	constructor(
+		type: string,
+		title: string
+	) {
+		this.type = type;
+		this.title = title;
+		this.data = new Array<Array<any>>();
+		this.columns = new Array<string>();
+		this.options = {
+			legend: { position: 'bottom', alignment: 'start' },
+			vAxis: {
+				title: 'Pain',
+				viewWindow: {
+					max: 5,
+					min: 0,
+				},
+				ticks: [0, 1, 2, 3, 4, 5]
+			}
+		};
+		this.timeSymptoms = new Map<string, ISymptomOverview[]>();
+		this.timeSymptomsOrder = new Array<string>();
+		this.lastSymptomMap = new Map<string, ISymptomLog>();
+	}
+
+	public update(symptoms$: Observable<ISymptom[]>, symptomMap: Map<string, string>, dayContent: DayViewModel): void {
+		symptoms$.subscribe(() => {
+			this.timeSymptoms = new Map<string, ISymptomOverview[]>();
+			this.timeSymptomsOrder = new Array<string>();
+			this.lastSymptomMap = new Map<string, ISymptomLog>();
+			this.columns.push('Time');
+			dayContent.symptoms.forEach(symptom => {
+				symptom.logs.forEach(
+					symptomLog => {
+						const symptomLabel = symptomMap.get(symptom.key);
+						if (!this.columns.includes(symptomLabel)) {
+							this.columns.push(symptomLabel);
+						}
+						if (!this.timeSymptoms.has(symptomLog.time)) {
+							this.timeSymptoms.set(symptomLog.time, new Array<ISymptomOverview>())
+							this.timeSymptomsOrder.push(symptomLog.time)
+						}
+						this.timeSymptoms.get(symptomLog.time).push(symptomLog);
+						this.lastSymptomMap.set(symptomLog.key, symptomLog);
+					}
+				);
+			});
+			this.data = new Array<Array<string | number>>();
+			for (const [time, symptomsEntries] of this.timeSymptoms.entries()) {
+				const entry = new Array<string | number>();
+				entry.push(time);
+				dayContent.symptoms.forEach(symptom => {
+					const registeredSymptom = symptomsEntries.find(s => s.key === symptom.key);
+					const registeredSymptomPain = registeredSymptom == null ?
+						-1 : registeredSymptom.pain;
+					entry.push(Number(registeredSymptomPain));
+				});
+				this.data.push(entry);
+			}
+			if (this.timeSymptoms.get(dayContent.wakeUp) == null) {
+				const wakeUpEntry = new Array<string | number>();
+				wakeUpEntry.push(dayContent.wakeUp);
+				dayContent.symptoms.forEach(() => {
+					wakeUpEntry.push(0);
+				});
+				this.data.push(wakeUpEntry);
+			}
+			this.data.sort();
+			if (this.timeSymptoms.get(dayContent.goToBed) == null) {
+				const bedTime = dayContent.goToBed > '00:00' && dayContent.goToBed < '07:00' ?
+					'23:59' : dayContent.goToBed;
+				const bedTimeEntry = new Array<string | number>();
+				bedTimeEntry.push(bedTime);
+				dayContent.symptoms.forEach(symptom => {
+					bedTimeEntry.push(this.lastSymptomMap.get(symptom.key).pain);
+				});
+				this.data.push(bedTimeEntry);
+			}
+			this.data.sort();
+			for (let i = 0; i < this.data.length; i++) {
+				for (let j = 1; j < this.data[i].length; j++) {
+					if (this.data[i][j] === -1) {
+						this.data[i][j] = i === 0 ? 0 : this.data[i - 1][j];
+					}
+				}
+			}
+		});
+	}
+}
--- front/src/app/ui/day/day.component.ts
+++ front/src/app/ui/day/day.component.ts
@@ -10,7 +10,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { ATimeComponent } from '../time';
 import { IDay } from 'src/app/models';
-import { ISymptomOverview, ISymptomLog } from 'src/app/models/symptom.model';
+import { DayChartViewModel } from 'src/app/models/chart.view.model';
 
 @Component({
 	selector: 'app-day',
@@ -23,25 +23,7 @@ export class DayComponent extends ATimeComponent implements OnInit {
 	public dayContent$: Subject<DayViewModel>;
 	public symptomMap: Map<string, string>;
 	public symptomPainColorMap: Map<number, string>;
-
-	public title = 'Symptoms';
-	public type = 'SteppedAreaChart';
-	public data = new Array<Array<any>>();
-	public columns = new Array<string>();
-	public options = {
-		legend: { position: 'bottom', alignment: 'start' },
-		vAxis: {
-			title: 'Pain',
-			viewWindow: {
-				max: 5,
-				min: 0,
-			},
-			ticks: [0, 1, 2, 3, 4, 5]
-		}
-	};
-	public timeSymptoms: Map<string, ISymptomOverview[]>;
-	public timeSymptomsOrder: string[];
-	public lastSymptomMap: Map<string, ISymptomLog>;
+	public steppedAreaChart: DayChartViewModel;
 
 	constructor(
 		public globalService: GlobalService,
@@ -57,6 +39,7 @@ export class DayComponent extends ATimeComponent implements OnInit {
 			this.dayContent = new DayViewModel(day);
 			this.dayContent$.next(this.dayContent);
 		}
+		this.steppedAreaChart = new DayChartViewModel('SteppedAreaChart', 'Symptoms');
 	}
 
 	public ngOnInit(): void {
@@ -65,71 +48,8 @@ export class DayComponent extends ATimeComponent implements OnInit {
 			day => {
 				this.dayContent = new DayViewModel(day);
 				this.dayContent$.next(this.dayContent);
-
-				this.symptoms$.subscribe(() => {
-					this.timeSymptoms = new Map<string, ISymptomOverview[]>();
-					this.timeSymptomsOrder = new Array<string>();
-					this.lastSymptomMap = new Map<string, ISymptomLog>();
-					this.columns.push('Time');
-					this.dayContent.symptoms.forEach(symptom => {
-						symptom.logs.forEach(
-							symptomLog => {
-								const symptomLabel = this.globalService.symptomMap.get(symptom.key);
-								if (!this.columns.includes(symptomLabel)) {
-									this.columns.push(symptomLabel);
-								}
-								if (!this.timeSymptoms.has(symptomLog.time)) {
-									this.timeSymptoms.set(symptomLog.time, new Array<ISymptomOverview>())
-									this.timeSymptomsOrder.push(symptomLog.time)
-								}
-								this.timeSymptoms.get(symptomLog.time).push(symptomLog);
-								this.lastSymptomMap.set(symptomLog.key, symptomLog);
-							}
-						);
-					});
-					this.data = new Array<Array<string | number>>();
-					for (const [time, symptomsEntries] of this.timeSymptoms.entries()) {
-						const entry = new Array<string | number>();
-						entry.push(time);
-						this.dayContent.symptoms.forEach(symptom => {
-							const registeredSymptom = symptomsEntries.find(s => s.key === symptom.key);
-							const registeredSymptomPain = registeredSymptom == null ?
-								-1 :
-								registeredSymptom.pain;
-							entry.push(registeredSymptomPain);
-						});
-						this.data.push(entry);
-					}
-					if (this.timeSymptoms.get(this.dayContent.wakeUp) == null) {
-						const wakeUpEntry = new Array<string | number>();
-						wakeUpEntry.push(this.dayContent.wakeUp);
-						this.dayContent.symptoms.forEach(() => {
-							wakeUpEntry.push(0);
-						});
-						this.data.push(wakeUpEntry);
-					}
-					this.data.sort();
-					if (this.timeSymptoms.get(this.dayContent.goToBed) == null) {
-						const bedTime = this.dayContent.goToBed > '00:00' && this.dayContent.goToBed < '07:00' ?
-							'23:59' : this.dayContent.goToBed;
-						const bedTimeEntry = new Array<string | number>();
-						bedTimeEntry.push(bedTime);
-						this.dayContent.symptoms.forEach(symptom => {
-							bedTimeEntry.push(this.lastSymptomMap.get(symptom.key).pain);
-						});
-						this.data.push(bedTimeEntry);
-					}
-					this.data.sort();
-					for (let i = 0; i < this.data.length; i++) {
-						for (let j = 1; j < this.data[i].length; j++) {
-							if (this.data[i][j] === -1) {
-								this.data[i][j] = i === 0 ? 0 : this.data[i - 1][j];
-							}
-						}
-					}
-				});
+				this.steppedAreaChart.update(this.symptoms$, this.globalService.symptomMap, this.dayContent);
 			}
 		);
-
 	}
 }
