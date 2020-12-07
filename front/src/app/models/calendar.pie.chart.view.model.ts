6d3ee6c212b8efd63a6fc4287b110e09125a6806
sebferrer <sebastien@sebferrer.fr>
2020-12-08 00:06:03 +0100

    calendar pie charts

---
+++ front/src/app/models/calendar.pie.chart.view.model.ts
@@ -0,0 +1,48 @@
+import { ISymptomOverview, ISymptomLog, ISymptom } from './symptom.model';
+import { DayOverviewViewModel } from 'src/app/models/day.overview.view.model';
+import { Observable } from 'rxjs';
+import { DayViewModel } from './day.view.model';
+import { AChart } from './chart.model';
+
+export class CalendarPieChartViewModel extends AChart {
+
+	private symptomKey: string;
+	private symptomPainMap: Map<number, number>;
+	private readonly NB_PAIN_LEVELS = 5;
+
+	constructor(
+		type: string,
+		symptomKey: string
+	) {
+		super(type, symptomKey);
+		this.type = type;
+		this.data = new Array<Array<string | number>>();
+		this.columns = new Array<string>();
+		this.options = {
+			pieHole: 0.4,
+			colors: ['#93EA84', '#BFBC00', '#FDEC05', '#FFC000', '#E40026', '#980019']
+		};
+		this.symptomPainMap = new Map<number, number>();
+		this.symptomKey = symptomKey;
+	}
+
+	public update(dayOverviews: DayOverviewViewModel[]): void {
+		this.columns = ['Pain', 'Frequency'];
+
+		dayOverviews.forEach(overview => {
+			const symptom = overview.symptomOverviews.find(s => s.key === this.symptomKey);
+			const symptomPain = symptom == null ? 0 : symptom.pain;
+			if (!this.symptomPainMap.has(symptomPain)) {
+				this.symptomPainMap.set(symptomPain, 1);
+			}
+			else {
+				this.symptomPainMap.set(symptomPain, this.symptomPainMap.get(symptomPain) + 1);
+			}
+		});
+
+		this.data = new Array<Array<string | number>>();
+		for (let i = 0; i < this.NB_PAIN_LEVELS + 1; i++) {
+			this.data.push([i.toString(), this.symptomPainMap.get(i) || 0]);
+		}
+	}
+}
--- front/src/app/ui/calendar/calendar.component.ts
+++ front/src/app/ui/calendar/calendar.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import { DaysService } from 'src/app/infra';
 import { getDetailedDate } from 'src/app/util/date.utils';
 import { DayOverviewViewModel } from 'src/app/models/day.overview.view.model';
 import { IDetailedDate } from 'src/app/models/detailed.date';
 import { GlobalService } from 'src/app/infra/global.service';
+import { CalendarPieChartViewModel } from 'src/app/models/calendar.pie.chart.view.model';
+import { ISymptom } from 'src/app/models/symptom.model';
 
 @Component({
 	selector: 'app-calendar',
@@ -15,17 +17,21 @@ export class CalendarComponent implements OnInit {
 
 	public overviews: DayOverviewViewModel[];
 	public overviews$: Subject<DayOverviewViewModel[]>;
+	public symptoms$: Observable<ISymptom[]>;
 	public month: number;
 	public year: number;
 	public symptomMap: Map<string, string>;
 	public symptomPainColorMap: Map<number, string>;
 	public monthMap: Map<number, string>;
 	public today: IDetailedDate;
+	public pieCharts: Map<string, CalendarPieChartViewModel>;
 
 	constructor(
 		public globalService: GlobalService,
 		private daysService: DaysService
-	) { }
+	) {
+		this.pieCharts = new Map<string, CalendarPieChartViewModel>();
+	}
 
 	public ngOnInit(): void {
 		this.overviews = new Array<DayOverviewViewModel>();
@@ -38,6 +44,16 @@ export class CalendarComponent implements OnInit {
 		this.symptomMap = this.globalService.symptomMap;
 		this.symptomPainColorMap =
 			new Map([[0, 'default'], [1, 'pain-1'], [2, 'pain-2'], [3, 'pain-3'], [4, 'pain-4'], [5, 'pain-5']]);
+		this.symptoms$ = this.globalService.symptoms$;
+	}
+
+	public updateCharts() {
+		this.symptoms$.subscribe(symptoms => {
+			symptoms.forEach(symptom => {
+				this.pieCharts.set(symptom.key, new CalendarPieChartViewModel('PieChart', symptom.key));
+				this.pieCharts.get(symptom.key).update(this.overviews);
+			});
+		})
 	}
 
 	public updateCalendar(month: number, year: number) {
@@ -48,6 +64,7 @@ export class CalendarComponent implements OnInit {
 					this.overviews.push(new DayOverviewViewModel(day));
 				});
 				this.overviews$.next(this.overviews);
+				this.updateCharts();
 			}
 		);
 	}
