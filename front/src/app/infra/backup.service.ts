51448e48c08fea8a18b69d8135bebed49c559e8b
sebferrer <sebastien@sebferrer.fr>
2020-12-25 20:05:41 +0100

    backup service forkJoin

--- front/src/app/infra/backup.service.ts
+++ front/src/app/infra/backup.service.ts
@@ -1,4 +1,4 @@
-import { Observable, of } from 'rxjs';
+import { Observable, of, forkJoin } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { IDay } from '../models';
 import { getFormattedDate } from 'src/app/util/date.utils';
@@ -7,30 +7,26 @@ import { IBackup } from '../models/backup.model';
 import { DaysService } from './days.service';
 import { SymptomsService } from './symptoms.service';
 import { SettingsService } from './settings.service';
+import { ISymptom } from '../models/symptom.model';
 
 @Injectable()
 export class BackupService {
 
 	constructor(
-		private daysSerice: DaysService,
+		private daysService: DaysService,
 		private symptomsService: SymptomsService,
 		private settingsService: SettingsService
 	) { }
 
 	public getBackup(): Observable<IBackup> {
-		return this.daysSerice.getDays().pipe(
-			mergeMap(days => {
-				return this.symptomsService.getSymptoms().pipe(
-					mergeMap(symptoms => {
-						return this.settingsService.getSettings().pipe(
-							map(settings => {
-								return { days, symptoms, settings }
-							})
-						)
-					})
-				)
+		return forkJoin([
+			this.daysService.getDays(),
+			this.symptomsService.getSymptoms(),
+			this.settingsService.getSettings()
+		]).pipe(
+			map(([days, symptoms, settings]) => {
+				return { days, symptoms, settings };
 			})
 		);
 	}
-
 }
